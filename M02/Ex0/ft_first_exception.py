de72753784f0c0036d7a1ff910a1603495c5f394
Victor Aguayo Alcazar <vaguayo-@cbr1s3.42barcelona.com>
2026-02-11 19:34:30 +0100

    Starting M02

--- M02/Ex0/ft_first_exception.py
+++ M02/Ex0/ft_first_exception.py
@@ -1,2 +1,13 @@
+def check_temperature(temp_str: str):
+    print("Testing temperature:", temp_str)
+    try:
+        num = int(temp_str)
+    except:
+        print(f"Error: '{temp_str}' is not a valid number")
+    if num > 40:
+        print(f"Error: {num}°C is too hot for plants (max 40°C)")
+def ft_first_exception() -> None:
+    
 if __name__ == "__main__":
-    ")
+    ft_first_exception()
+   
